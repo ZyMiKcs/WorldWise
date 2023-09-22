96851a02e4f28beae82bbad1bdb1a974fe256fd2
zymikcs <zymik.official@gmail.com>
2023-09-22 16:54:43 +0300

    Some optimization

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,10 @@
-import { createContext, useContext, useEffect, useReducer } from 'react';
+import {
+    createContext,
+    useCallback,
+    useContext,
+    useEffect,
+    useReducer,
+} from 'react';
 
 const BASE_URL = 'http://localhost:8000';
 
@@ -58,9 +64,6 @@ function CitiesProvider({ children }) {
         reducer,
         initialState
     );
-    // const [cities, setCities] = useState([]);
-    // const [isLoading, setIsLoading] = useState(false);
-    // const [currentCity, setCurrentCity] = useState({});
 
     useEffect(function () {
         async function fetchCities() {
@@ -79,21 +82,24 @@ function CitiesProvider({ children }) {
         fetchCities();
     }, []);
 
-    async function getCity(id) {
-        if (+id === currentCity.id) return;
+    const getCity = useCallback(
+        async function getCity(id) {
+            if (+id === currentCity.id) return;
 
-        dispatch({ type: 'loading' });
-        try {
-            const res = await fetch(`${BASE_URL}/cities/${id}`);
-            const data = await res.json();
-            dispatch({ type: 'city/loaded', payload: data });
-        } catch {
-            dispatch({
-                type: 'rejected',
-                payload: 'There was an error loading data...',
-            });
-        }
-    }
+            dispatch({ type: 'loading' });
+            try {
+                const res = await fetch(`${BASE_URL}/cities/${id}`);
+                const data = await res.json();
+                dispatch({ type: 'city/loaded', payload: data });
+            } catch {
+                dispatch({
+                    type: 'rejected',
+                    payload: 'There was an error loading data...',
+                });
+            }
+        },
+        [currentCity.id]
+    );
 
     async function createCity(newCity) {
         try {
